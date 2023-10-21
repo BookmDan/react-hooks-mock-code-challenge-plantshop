dd29e720fe6ccff8860983f5733867f62bfd2fd4
BookmDan <danoh329@gmail.com>
2023-10-21 16:51:36 -0700

    I added text to the README file

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,23 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({ plants, searchTerm }) {
+  if (!plants || plants.length === 0) {
+    return <div>No plants available.</div>;
+  }
+
+  const filteredPlants = plants.filter(plantObj => {
+    // check name of plant, reurn true if it matches the search term 
+    return plantObj.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  console.log(filteredPlants)
+  
+  const plantCards = filteredPlants.map((plant) => {
+    return <PlantCard key={plant.id}  plant={plant}  />
+  })
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantCards}</ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const[searchTerm, setSearchTerm] = useState("")
+  console.log(plants);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then((plantsArray) => {
+        setPlants(plantsArray)
+        // console.log(plantsArray)
+      })
+  }, [])
+
+  //sending newPlant from NewPlantForm
+  //1. define callback function
+  function handleAddNewPlant(newPlant) {
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+    // console.log(newPlant)
+  }
+
+  function handleUpdateSearch(newSearch) {
+    setSearchTerm(newSearch)
+  }
+      // pass callback as a prop
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddNewPlant={ handleAddNewPlant} />
+      <Search searchTerm={searchTerm} onUpdateSearch={handleUpdateSearch} />
+      <PlantList plants={plants} searchTerm={searchTerm} />
     </main>
   );
 }
